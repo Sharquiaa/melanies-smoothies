018767bb2302ac5c9514302f5b414eca9fb91dc3
Sharquiaa <92844554+Sharquiaa@users.noreply.github.com>
2024-11-27 22:56:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -24,24 +25,24 @@ if ingredients_list:
     ingredients_string = ''
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
+        #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
+        #st.write(my_insert_stmt)
 
-    time_to_insert = st.button('Submit Order')
+        time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
         
-        st.success(f"Your Smoothie is ordered! {name_on_order}",icon="✅")
+            st.success(f"Your Smoothie is ordered! {name_on_order}",icon="✅")
 
 
 #New section to display smoothiefroot nutrition information
-import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response).json()
 sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
